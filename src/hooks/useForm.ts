95146dc36b1193a32fc96fcd0152f52dae8b1c6c
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 12:41:57 -0600

    [FEAT] Multiple changes to the useForm method
    - Creating the FormState interface to define the generic type and assign values ​​to dynamic properties
    - Inheriting from FormState to guarantee string values
    - Expecting initialForm to be of the generic type T
    - Defining the form state with the value of initialForm
    - Using the setForm function to update the form's value, where I obtained the previous values ​​and propagated them to avoid losing any. I used dynamic properties to update the form's values ​​using their key and value.
    - Returning the reference to the handleChange and form functions

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,12 +1,22 @@
 import { useState } from "react"
+interface FormState<T>{
+    [key:string]:T
+}
 
-export const useForm = () =>{
+export const useForm = <T extends FormState<string>>(initialForm: T) =>{
+    const [form, setForm] = useState(initialForm);
 
     const handleChangle = (e:React.ChangeEvent<HTMLInputElement>)  => {
         const { name, value } = e.target;
+
+        setForm(prevState => ({
+            ...prevState,
+            [name]:value
+        }));
     }
 
     return {
-
+        handleChangle,
+        form
     }
 }

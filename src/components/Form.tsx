ec632159a83f979eeb676babebb7436936ca8ee2
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 11:18:26 -0600

    [FEAT] setting the h-screen prop so that the form occupies the entire space of the container

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,7 @@
 
 export default function Form() {
   return (
-    <form className="w-full border-2 border-blue-400">
+    <form className="w-full h-screen border-2 border-blue-400">
 
     </form>
   )

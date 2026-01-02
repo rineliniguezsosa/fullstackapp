314285f6f28e266d8b7dee78c255707aa8bfc13e
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 11:11:19 -0600

    [FEAT] Importing the Head component and setting it in the application, using the flex-column prop of display flex so that the children are vertically aligned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import  Head  from '../components/Head'
 
 export default function Home() {
   return (
-    <div className="flex h-screen border-2 border-red-500 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    
+    <div className="flex flex-col h-screen border-2 border-red-500 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <Head/>
     </div>
   );
 }

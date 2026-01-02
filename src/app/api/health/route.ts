374a4569388e8e8534f0bd0684a2a25759e0737f
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 12:10:05 -0600

    [FEAT] returning the GET function to the nextjs convention, establishing semicolons

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    return NextResponse.json({working:"OK"})
-}
+export const GET = async () => {
+    return NextResponse.json({working:"OK"});
+};

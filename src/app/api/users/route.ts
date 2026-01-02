24e8295dbd6962e87881a1fbf5060888ba83d5da
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 13:28:49 -0600

    [FEAT] We import the connection pool to perform queries.
    - Use `pool.query` to establish an SQL query to insert records into the `users` table.

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,12 @@
+import { pool } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(request:Request){
     const { name, email } = await request.json();
+
+    const { rows } = await pool.query(
+    'INSERT INTO users (name,email) VALUES ($1, $2) RETURNING *',
+    [name,email]
+  );
   return NextResponse.json({msj:"Aqui obtengo los usuarios"});
 }

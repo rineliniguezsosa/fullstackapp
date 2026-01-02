7960988e9773e56796d0a97f24de4a7abc52a138
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 14:41:48 -0600

    [FEAT] Changes to Page
    - Importing the React hooks useEffect and useState
    - Creating the users state, typed as User[] and initialized as an empty array
    - Setting the useEffect function to call the getUsers method, which in turn calls the API to retrieve the users and then saves them to users using the setUsers function
    - Iterating through the users using map in an ordered list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,37 @@
 
 import  Head  from '../components/Head'
 import Form from '../components/Form'
+import { useEffect, useState } from 'react'
+import { User } from '@/types/user'
+
 
 export default function Home() {
+
+   const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await fetch("/api/users")
+      const data = await res.json()
+      setUsers(data)
+    }
+
+    getUsers()
+  }, [])
+
   return (
-    <div className="flex flex-col h-screen border-2 border-red-500 items-center justify-center font-sans dark:bg-black">
+    <div className="flex flex-col h-screen items-center justify-center font-sans dark:bg-black">
         <Head/>
 
         <Form/>
+
+          <ul>
+            {users.map((u) => (
+              <li key={u.id}>
+                {u.name} - {u.email}
+              </li>
+            ))}
+          </ul>
     </div>
   );
 }

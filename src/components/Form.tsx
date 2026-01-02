2e836ab78730ae4c3d53cca583b672b69f659a54
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 12:55:31 -0600

    [FEAT] Multiple changes in Form.tsx
    - Importing the custom hook useForm
    - Destructuring useForm: form, handleChange to listen for changes, submitForm to submit form data
    - Setting the object with an empty name prop to initialize the state
    - Setting the onSubmit prop to submit form information
    - Setting the flex-col prop so that the inputs are vertical
    - Setting the HTML input element with the props type="text" as a text input field, name as an identifier equal to "name", id = "name" with a unique identifier, value equal to form.name to set the dynamic value, and onchange passing the handleChange reference to listen for input changes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,19 @@
+import { useForm } from "@/hooks/useForm"
 
 
 export default function Form() {
+    const { form,handleChange,submitForm} = useForm({
+        nombre:''
+    })
   return (
-    <form className="w-1/2 h-screen m-2 flex items-center justify-center border-2 border-blue-400">
-
+    <form onSubmit={submitForm} className="w-1/2 h-screen m-2 flex flex-col items-center justify-center border-2 border-blue-400">
+        <input 
+            type="text" 
+            name="name"
+            id="name"
+            value={form.nombre}
+            onChange={handleChange}
+        />
     </form>
   )
 }

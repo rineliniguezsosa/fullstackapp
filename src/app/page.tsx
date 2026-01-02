f888badd0d1d1f3181a3c2c10cf8ad6e9b1b2d67
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 11:19:21 -0600

    [FEAT] importing the Form component and setting it in the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import  Head  from '../components/Head'
+import Form from '../components/Form'
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen border-2 border-red-500 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <Head/>
+
+        <Form/>
     </div>
   );
 }

539df7e7a107f0bd7c4c909dd8e3c4f3737d0b26
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 12:28:22 -0600

    [FEAT] Destructuring the name and value of the form's input event to read input values ​​using dynamic properties

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -3,7 +3,7 @@ import { useState } from "react"
 export const useForm = () =>{
 
     const handleChangle = (e:React.ChangeEvent<HTMLInputElement>)  => {
-        
+        const { name, value } = e.target;
     }
 
     return {

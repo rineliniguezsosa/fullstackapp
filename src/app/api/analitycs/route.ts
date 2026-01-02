f135a3818dee9bb0cf1841525f6c6e526968c4ec
Rinel <rineliniguezsosa@gmail.com>
2026-01-02 14:04:54 -0600

    [FEAT] - Correcting the import of NextRequest to NextResponse - Creating the GET function to obtain the number of users - I created the variable `result` where I used the pool to create a query that simply counts all user records - I set the alias to `total_users` - We return the response using `NextResponse` within an object called `totalusers`, where we get the first position from `result` and obtain the alias `total_users`

--- src/app/api/analitycs/route.ts
+++ src/app/api/analitycs/route.ts
@@ -1,2 +1,10 @@
 import { pool } from "@/lib/db";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    const result = await pool.query(
+        "SELECT COUNT(*) AS total_users FROM users"
+    );
+
+    return NextResponse.json({totalusers:result.rows[0].total_users})
+}
